66a7fee955d917a8706972b7e1f8a524b2c2044d
amex-imd <amexpimd@gmail.com>
2026-02-01 16:32:04 +0300

    Python: The method 'is_palindrome' was added

--- src/python/mathtools.py
+++ src/python/mathtools.py
@@ -3,9 +3,13 @@ __EPSILON: float = 1e-12
 def reverse(val: int) -> int:
     res: int = 0
     sign: int = -1 if val < 0 else 1
-    x *= sign
-    while x > 0:
-        res = res * 10 + x % 10
-        x //= 10
+    val *= sign
+    while val > 0:
+        res = res * 10 + val % 10
+        val //= 10
     res *= sign
     return res
+
+def is_palindrome(val: int) -> bool:
+    if val < 0: return False
+    return val == reverse(val)
