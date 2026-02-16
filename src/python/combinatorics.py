6d2069c811144e4f3126cce051471bc26c7286a4
amex-imd <amexpimd@gmail.com>
2026-02-16 23:02:55 +0300

    The methods 'PascalTriangle' and 'PasalTriangleRow' were added

--- src/python/combinatorics.py
+++ src/python/combinatorics.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 def factorial(number: int):
     if number == 0 or number == 1: return 1
@@ -7,3 +7,18 @@ def factorial(number: int):
         res *= i
     return res
 
+def PascalTriangle(rows: int) -> List[List[int]]:
+    res: List[List[int]] = []
+    for i in range(rows):
+        tmp: List[int] = [1] * (i+1)
+        for j in range(1, i-1):
+            tmp[j] = res[i][j-1] + res[i][j]
+        res.append(tmp)
+    return res
+
+def PasalTriangleRow(rowIndex: int) -> List[int]:
+    res: List[int] = [1]
+    for i in range(rowIndex):
+        nextElem = res[i] * (rowIndex - i) // (i+1)
+        res.append(nextElem)
+    return res
